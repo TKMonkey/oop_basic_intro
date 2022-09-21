c6d57b47799c13cf91d66119da70e098cf81cf3e
Juan E Quintero R <juequinterore666@gmail.com>
2022-09-21 15:06:14 -0500

    feat: add screen class

---
+++ src/oop/screen.ts
@@ -0,0 +1,12 @@
+/**
+ * Device to show messages
+ */
+export class Screen {
+  /**
+   * Shows a message
+   * @param message String to be shown
+   */
+  public showMessage(message: string) {
+    console.log(message);
+  }
+}
